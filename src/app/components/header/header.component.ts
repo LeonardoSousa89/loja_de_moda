d2d1458338f784f5de63edfecad30126aeda5435
Mendes Sousa <leoprofessionallogytech@outlook.com>
2023-02-25 01:07:28 -0300

    '@v1.0.10'

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+  private window_document: Window=window
+  private URLs={
+    whatsapp: 'https://wa.me/message/N767K3D4E7TAG1'
+  }
+
+  ngOnInit(): void {}
+
+  public targetLink(link: string): void{
+    this.window_document.open(link)
+  }
+
+  public targetWhatsapp(): void{
+    const { whatsapp }=this.URLs
+    
+    this.targetLink(whatsapp)
+  }
 }
