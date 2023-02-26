d45942af03f917c5e1b64cab8e10fccb4982a3a6
Mendes Sousa <leoprofessionallogytech@outlook.com>
2023-02-26 18:25:48 -0300

    '@v1.0.13'

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+  
+  private social={
+    facebook:'https://www.facebook.com/profile.php?id=100087040453902',
+    github: 'https://github.com/LeonardoSousa89',
+    linkedin: 'https://www.linkedin.com/in/leonardo-dos-santos-sousa-238651173'
+  }
+
+  private window_document: Window=window
+
+  ngOnInit(): void {}
+
+  public targetLink(link: string): void{ this.window_document.open(link) }
+
+  public facebookTarget(): void{ this.targetLink(this.social.facebook) }
+  public githubTarget():   void{ this.targetLink(this.social.github)   }
+  public linkedinTarget(): void{ this.targetLink(this.social.linkedin) }
 
 }
